10b9550527372a897e75bede3df774d5e4873d8e
Rio <xuyirio@163.com>
2015-05-03 22:37:58 -0700

    DT

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -29,5 +29,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
+print len(features_train[0])
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print acc
 
