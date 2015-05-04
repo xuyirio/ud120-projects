5b29b006591ed3f0c16e8d02d1187596b8c56daf
Rio <xuyirio@163.com>
2015-05-03 23:06:15 -0700

    feature

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -7,8 +7,8 @@ numpy.random.seed(42)
 
 ### the words (features) and authors (labels), already largely processed
 ### these files should have been created from the previous (Lesson 10) mini-project.
-words_file = "../text_learning/word_data_overfit.pkl" 
-authors_file = "../text_learning/email_authors_overfit.pkl"
+words_file = "../text_learning/your_word_data.pkl"
+authors_file = "../text_learning/your_email_authors.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
 
@@ -35,6 +35,20 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred, labels_test)
 
+n = 0
+for i in range(len(clf.feature_importances_)):
+  if clf.feature_importances_[i] >= 0.2:
+    n = i
+    print clf.feature_importances_[i], i  # 0.7647, 33604
 
+feature_names = vectorizer.get_feature_names()
+print len(feature_names)
+print feature_names[n]
