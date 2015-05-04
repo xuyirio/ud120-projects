246e8017e4cbf220ee3e4e1afcd5e24db8c794a2
Rio <xuyirio@163.com>
2015-05-03 23:06:54 -0700

    validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -27,6 +27,15 @@ labels, features = targetFeatureSplit(data)
 
 
 
-### it's all yours from here forward!  
+### it's all yours from here forward!
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(
+    features, labels, test_size=0.3, random_state=42)
 
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred, labels_test)
