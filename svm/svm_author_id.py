8e6ec10948668ce37402ebe88e548343b3b0ab6e
Rio <xuyirio@163.com>
2015-05-03 22:41:39 -0700

    SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -28,5 +28,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
+from sklearn.svm import SVC
+clf = SVC(C=10000.0,kernel='rbf')
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(pred, labels_test))
+cnt = 0
+for item in pred:
+  if item == 1:
+    cnt = cnt+1
+print(cnt)
+
 
 
